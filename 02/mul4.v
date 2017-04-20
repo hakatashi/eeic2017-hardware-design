3292acd5c76dd5a2a06b9b92e9de71bf11f9c8bb
Koki Takahashi <hakatasiloving@gmail.com>
2017-04-21 02:52:17 +0900

    Add mul4.v and sub4.v

---
+++ 02/mul4.v
@@ -0,0 +1,5 @@
+module mul4(result, A, B);
+	output [7:0] result;
+	input [3:0] A, B;
+	assign result = A * B;
+endmodule
---
+++ 02/sub4.v
@@ -0,0 +1,5 @@
+module sub4(result, A, B);
+	output [4:0] result;
+	input [3:0] A, B;
+	assign result = A - B;
+endmodule
